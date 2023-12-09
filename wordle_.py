30185d276dfedd26dafb3b25373500dc76fa5b30
PalinaK <polina.kirichencko@yandex.ru>
2023-12-09 20:12:16 +0100

    list of words changed

--- wordle_.py
+++ wordle_.py
@@ -3,25 +3,25 @@ from colorama import init
 init()
 from colorama import Fore, Back, Style
 
-#файл со словами, откуда вы будете случайным образом выбирать слово для игры.
-with open('example,txt', 'r+', encoding='utf-8') as f:
+
+with open('example.txt', 'r+', encoding='utf-8') as f:
     words_list = f.read().split()
-    length_5 = [i for i in words_list if len(i) == 5] #нужно сперва исключить слова, длина которых не равна 5
-    word = random.choice(length_5).upper()  #рандомно выбираем загаданное слово из списка в верхнем регистре
-    count = 0 #счетчик попыток
-    while count < 6:  #имеем 6 попыток
-        text = input().upper() #вводим слово
-        if text.lower() in length_5: #проверяем, существует ли слово(=есть ли в файле)
+    length_5 = [i for i in words_list if len(i) == 5]
+    word = random.choice(length_5).upper()
+    count = 0
+    while count < 6:
+        text = input().upper()
+        if text.lower() in length_5:
             count_yel = 0
             for i in range(len(text)):
-                if text[i] not in word:  #буквы нет в загаданном слове
-                    print(Fore.LIGHTWHITE_EX + Back.LIGHTBLACK_EX + Style.BRIGHT + text[i], end=' ')  #на сером фоне белая буква
-                else:  #буква есть в загаданном слове
-                    if word.find(text[i]) == i: #совпало по индексу
-                        print(Fore.LIGHTBLACK_EX + Back.LIGHTYELLOW_EX + Style.BRIGHT + text[i], end=' ')  # на желтом фоне черная буква
+                if text[i] not in word:
+                    print(Fore.LIGHTWHITE_EX + Back.LIGHTBLACK_EX + Style.BRIGHT + text[i], end=' ')
+                else:
+                    if word.find(text[i]) == i:
+                        print(Fore.LIGHTBLACK_EX + Back.LIGHTYELLOW_EX + Style.BRIGHT + text[i], end=' ')
                         count_yel += 1
-                    else:  #не совпало по индексу
-                        print(Fore.LIGHTBLACK_EX + Back.LIGHTWHITE_EX + Style.BRIGHT + text[i], end=' ')  # на белом фоне черная буква
+                    else:
+                        print(Fore.LIGHTBLACK_EX + Back.LIGHTWHITE_EX + Style.BRIGHT + text[i], end=' ')
             if count_yel == 5:
                 print(Fore.BLACK + Back.CYAN + Style.BRIGHT + 'Вы выиграли, поздравляем!')
                 break
